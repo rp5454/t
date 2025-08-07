87b37460247a6e6e61726ee89ad0b5f56ed73b6b
Rishabh Pathak <104138852+rp5454@users.noreply.github.com>
2025-08-07 22:44:57 +0530

    Create c2d.js

---
+++ c2d.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function CustomerDetails({ customer }) {
+  return (
+    <div style={{ flex: 1 }}>
+      <h3>Customer Details</h3>
+      {customer ? (
+        <div>
+          <p><strong>ID:</strong> {customer.id}</p>
+          <p><strong>First Name:</strong> {customer.firstName}</p>
+          <p><strong>Last Name:</strong> {customer.lastName}</p>
+          <p><strong>Email:</strong> {customer.email}</p>
+        </div>
+      ) : (
+        <p>No customer selected</p>
+      )}
+    </div>
+  );
+}
+
+export default CustomerDetails;
