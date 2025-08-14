84ac0b8c97c0689f9573c76bbb5f15eec181777e
Rishabh Pathak <104138852+rp5454@users.noreply.github.com>
2025-08-14 16:03:29 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+// src/App.js
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import ViewApplications from "./pages/Applications/ViewApplications";
+import DeleteApplication from "./pages/Applications/DeleteApplication";
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Applications list page */}
+        <Route path="/" element={<Navigate to="/applications" />} />
         <Route path="/applications" element={<ViewApplications />} />
-        
-        {/* Default route */}
+        <Route path="/applications/delete/:id" element={<DeleteApplication />} />
         <Route path="*" element={<p>Page Not Found</p>} />
       </Routes>
     </Router>
